5c17e6ba9dac7512a65f1447b58a30c77a5ee17a
Elran <elran3000@gmail.com>
2018-10-14 19:25:15 +0300

    Heroku setup

---
+++ server/server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (error) => {
+    res.status(400).send(error);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Started at port ${port}`);
+});
+
+module.exports = {app};
