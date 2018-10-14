37dc87ef1ea873f0eb64cdf5bcbd10d8eb28ae27
Elran <elran3000@gmail.com>
2018-10-14 23:41:15 +0300

    changed hello

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.get('/',(req,res) =>{
-	res.send("HELLO")
+	res.send("HELLO" + req)
 });
 
 app.post('/todos', (req, res) => {
